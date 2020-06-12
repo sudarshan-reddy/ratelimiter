f987f0990a51997ffef9b2538f881ec54ec0f9d4
Sudarsan <sudar.theone@gmail.com>
2020-06-12 22:05:33 +0800

    added slack and per_request durations

--- src/leaky.rs
+++ src/leaky.rs
@@ -10,17 +10,24 @@ struct State {
 #[allow(dead_code)]
 pub struct Leaky {
     state: AtomicPtr<State>,
+
+    per_request: Duration,
+    max_slack: Duration,
 }
 
 #[allow(dead_code)]
 impl Leaky {
-    pub fn new() -> Self {
+    pub fn new(rate: u64) -> Self {
         let initial_state = &mut State {
             last: Instant::now(),
             sleep_for: Duration::new(0, 0),
         };
 
         let atomic_ptr = AtomicPtr::new(initial_state);
-        Leaky { state: atomic_ptr }
+        Leaky {
+            state: atomic_ptr,
+            per_request: Duration::new(1 / rate, 0),
+            max_slack: Duration::new(10 / rate, 0),
+        }
     }
 }
